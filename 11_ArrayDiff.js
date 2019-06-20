c227055ad4b62fb769539cc98608d21f012403dd
Leticia <lzduque@hotmail.com>
2019-06-20 10:22:38 -0400

    11

--- 11_ArrayDiff.js
+++ 11_ArrayDiff.js
@@ -1,3 +1,7 @@
 function array_diff(a, b) {
-
+  let result = a.filter(element => !b.includes(element));
+  return result;
 }
+
+console.log(array_diff([], [4,5]), [], "a was [], b was [4,5]");
+console.log(array_diff([3,4], [3]), [4], "a was [3,4], b was [3]");

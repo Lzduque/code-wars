6b5984ef18b211503d19b1eb3b7a1bfc7b980e85
Leticia <lzduque@hotmail.com>
2019-06-23 10:59:49 -0400

    12 refactor

--- 12_CalculatingwithFunctions.js
+++ 12_CalculatingwithFunctions.js
@@ -1,72 +1,32 @@
 function zero(x = 0) {
-  if (x === 0) {
-    return "0"
-  } else {
-    return eval("0".concat(x));
-  }
+  return (x === 0 ? "0" : eval("0".concat(x)));
 }
 function one(x = 0) {
-  if (x === 0) {
-    return "1"
-  } else {
-    return eval("1".concat(x));
-  }
+  return (x === 0 ? "1" : eval("1".concat(x)));
 }
 function two(x = 0) {
-  if (x === 0) {
-    return "2"
-  } else {
-    return eval("2".concat(x));
-  }
+  return (x === 0 ? "2" : eval("2".concat(x)));
 }
 function three(x = 0) {
-  if (x === 0) {
-    return "3"
-  } else {
-    return eval("3".concat(x));
-  }
+  return (x === 0 ? "3" : eval("3".concat(x)));
 }
 function four(x = 0) {
-  if (x === 0) {
-    return "4"
-  } else {
-    return eval("4".concat(x));
-  }
+  return (x === 0 ? "4" : eval("4".concat(x)));
 }
 function five(x = 0) {
-  if (x === 0) {
-    return "5"
-  } else {
-    return eval("5".concat(x));
-  }
+  return (x === 0 ? "5" : eval("5".concat(x)));
 }
 function six(x = 0) {
-  if (x === 0) {
-    return "6"
-  } else {
-    return eval("6".concat(x));
-  }
+  return (x === 0 ? "6" : eval("6".concat(x)));
 }
 function seven(x = 0) {
-  if (x === 0) {
-    return "7"
-  } else {
-    return eval("7".concat(x));
-  }
+  return (x === 0 ? "7" : eval("7".concat(x)));
 }
 function eight(x = 0) {
-  if (x === 0) {
-    return "8"
-  } else {
-    return eval("8".concat(x));
-  }
+  return (x === 0 ? "8" : eval("8".concat(x)));
 }
 function nine(x = 0) {
-  if (x === 0) {
-    return "9"
-  } else {
-    return eval("9".concat(x));
-  }
+  return (x === 0 ? "9" : eval("9".concat(x)));
 }
 
 function plus(x) {

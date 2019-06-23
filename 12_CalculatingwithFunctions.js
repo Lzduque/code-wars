ede81b29cfac2b00010e301dce37ae8c1106d22f
Leticia <lzduque@hotmail.com>
2019-06-23 11:03:33 -0400

    12 adjust Math.floor

--- 12_CalculatingwithFunctions.js
+++ 12_CalculatingwithFunctions.js
@@ -1,32 +1,32 @@
 function zero(x = 0) {
-  return (x === 0 ? "0" : eval("0".concat(x)));
+  return (x === 0 ? "0" : Math.floor(eval("0".concat(x))));
 }
 function one(x = 0) {
-  return (x === 0 ? "1" : eval("1".concat(x)));
+  return (x === 0 ? "1" : Math.floor(eval("1".concat(x))));
 }
 function two(x = 0) {
-  return (x === 0 ? "2" : eval("2".concat(x)));
+  return (x === 0 ? "2" : Math.floor(eval("2".concat(x))));
 }
 function three(x = 0) {
-  return (x === 0 ? "3" : eval("3".concat(x)));
+  return (x === 0 ? "3" : Math.floor(eval("3".concat(x))));
 }
 function four(x = 0) {
-  return (x === 0 ? "4" : eval("4".concat(x)));
+  return (x === 0 ? "4" : Math.floor(eval("4".concat(x))));
 }
 function five(x = 0) {
-  return (x === 0 ? "5" : eval("5".concat(x)));
+  return (x === 0 ? "5" : Math.floor(eval("5".concat(x))));
 }
 function six(x = 0) {
-  return (x === 0 ? "6" : eval("6".concat(x)));
+  return (x === 0 ? "6" : Math.floor(eval("6".concat(x))));
 }
 function seven(x = 0) {
-  return (x === 0 ? "7" : eval("7".concat(x)));
+  return (x === 0 ? "7" : Math.floor(eval("7".concat(x))));
 }
 function eight(x = 0) {
-  return (x === 0 ? "8" : eval("8".concat(x)));
+  return (x === 0 ? "8" : Math.floor(eval("8".concat(x))));
 }
 function nine(x = 0) {
-  return (x === 0 ? "9" : eval("9".concat(x)));
+  return (x === 0 ? "9" : Math.floor(eval("9".concat(x))));
 }
 
 function plus(x) {

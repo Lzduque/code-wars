4162d9b9443f06e464e0cdb130cdbe28e5a034f4
Leticia <lzduque@hotmail.com>
2019-06-30 19:05:55 -0400

    18

--- 18_RGBtoHexConversion.rb
+++ 18_RGBtoHexConversion.rb
@@ -1,5 +1,11 @@
 def rgb(r, g, b)
   def to_hex(n)
+    if n < 0
+      n = 0
+    end
+    if n > 255
+      n = 255
+    end
     n.to_s(16).rjust(2, '0').upcase
   end
   "#{to_hex r}#{to_hex g}#{to_hex b}"

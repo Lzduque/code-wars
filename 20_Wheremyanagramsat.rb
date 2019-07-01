30a8e545a7144a0434e86de62683a93caa5bebf5
Leticia <lzduque@hotmail.com>
2019-07-01 16:43:48 -0400

    20

--- 20_Wheremyanagramsat.rb
+++ 20_Wheremyanagramsat.rb
@@ -1,6 +1,11 @@
 def anagrams(word, words)
   word_hash = {}
   word.each do |letter|
-    if word_hash
+    if word_hash.key?(letter)
+      word_hash.letter += 1
+    else
+      word_hash.letter = 1
+    end
   end
+
 end

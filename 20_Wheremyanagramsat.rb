d04cd53aa7742eedd5a628c8bc042fbe4276f6c2
Leticia <lzduque@hotmail.com>
2019-07-04 14:38:29 -0400

    20

--- 20_Wheremyanagramsat.rb
+++ 20_Wheremyanagramsat.rb
@@ -1,11 +1,29 @@
 def anagrams(word, words)
   word_hash = {}
-  word.each do |letter|
+  word.split(//).each do |letter|
     if word_hash.key?(letter)
-      word_hash.letter += 1
+      word_hash[letter] += 1
     else
-      word_hash.letter = 1
+      word_hash[letter] = 1
     end
   end
-
+  response = []
+  words.each do |word|
+    word_anagram_hash = {}
+    word.split(//).each do |letter|
+      if word_anagram_hash.key?(letter)
+        word_anagram_hash[letter] += 1
+      else
+        word_anagram_hash[letter] = 1
+      end
+    end
+    if word_anagram_hash == word_hash
+      response << word
+    end
+  end
+  response
 end
+
+puts anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa']
+# puts anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer']
+# puts anagrams('laser', ['lazing', 'lazy',  'lacer']), []

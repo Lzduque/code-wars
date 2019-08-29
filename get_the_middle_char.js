b28b27a48a58598faf86151009cd5fbe620671c3
Leticia Zamolo Duque <lzduque@hotmail.com>
2019-08-29 01:50:38 +0000

    get the middle char

---
+++ get_the_middle_char.js
@@ -0,0 +1,17 @@
+function getMiddle(s) {
+    //  split word into letters
+    //  check if its odd or even
+    //  return one middle letter if it's odd
+    //  return the 2 middle letters if it's even
+    let arr = s.split("");
+    if (arr.length % 2 == 0) {
+      return arr[(arr.length/2) - 1] + arr[(arr.length/2)]
+    } else {
+      return arr[Math.round(arr.length/2) -1]
+    }
+  }
+
+console.log(getMiddle("test"), "es");
+console.log(getMiddle("testing"), "t");
+console.log(getMiddle("middle"), "dd");
+console.log(getMiddle("A"), "A");

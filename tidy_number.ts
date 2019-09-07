9f3f80bb56c3e438c22d6b2cbab19b125a4e6e84
Leticia <lzduque@hotmail.com>
2019-09-07 12:28:59 -0400

    tidy number

---
+++ tidy_number.ts
@@ -0,0 +1,15 @@
+function tidyNumber(num: number): boolean {
+  // transform number in an array
+  // compare this array to a sorted array
+  // if it is equal is tidy, if not, return false
+  let array: number[] = num.toString().split("").map((string) => Number(string));
+  // console.log("array: ", array.join(""));
+  // console.log("array.sort: ", array.sort((a, b) => a - b).join(""));
+  return array.join("") === array.sort((a, b) => a - b).join("")
+}
+
+console.log(tidyNumber(12), true);
+console.log(tidyNumber(102), false);
+console.log(tidyNumber(9672), false);
+console.log(tidyNumber(2789), true);
+console.log(tidyNumber(2335), true);

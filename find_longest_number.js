afd64f6258d1179d7be5cb90769aff792028191d
Leticia <lzduque@hotmail.com>
2019-09-06 23:13:06 -0400

    find longest number

---
+++ find_longest_number.js
@@ -0,0 +1,7 @@
+function findLongest(array) {
+    return 123;
+  }
+
+console.log(findLongest([1, 10, 100]), 100);
+console.log(findLongest([9000, 8, 800]), 9000);
+console.log(findLongest([8, 900, 500]), 900);

5bf6928b426f67daf4055a78117e221f96f6349a
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-05-27 14:04:50 -0400

    refactor

--- seven.js
+++ seven.js
@@ -1,14 +1,17 @@
-function seven(m) {
+const seven = (m) => sevenSteps(m, 0);
+
+const sevenSteps = (m, steps) => {
+    if (m < 100) return [m, steps];
+
     let digits = m.toString().split('');
+    const lastDigit = Number(digits.pop());
+    const firstDigits = Number(digits.join(''));
+    digits = (firstDigits - (2 * lastDigit)).toString().split('');
 
-    for (var step = 0; digits.length > 2; step++) {
-        const lastDigit = Number(digits.pop());
-        const firstDigits = Number(digits.join(''));
-        digits = (firstDigits - (2 * lastDigit)).toString().split('');;
-    }
+    return sevenSteps(Number(digits.join('')), steps + 1);
+};
 
-    return [Number(digits.join('')), step];
-}
+
 
 console.log(seven(371), [35, 1]);
 console.log(seven(1021), [10, 2]);

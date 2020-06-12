d841bed8f9e83609d2ee9364eb72093d7f283ad8
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-06-12 12:19:19 -0400

    Update wordsToMarks.js

--- wordsToMarks.js
+++ wordsToMarks.js
@@ -1,38 +1,7 @@
 function wordsToMarks(string) {
-    const alphabet = {
-        "a": 1,
-        "b": 2,
-        "c": 3,
-        "d": 4,
-        "e": 5,
-        "f": 6,
-        "g": 7,
-        "h": 8,
-        "i": 9,
-        "j": 10,
-        "k": 11,
-        "l": 12,
-        "m": 13,
-        "n": 14,
-        "o": 15,
-        "p": 16,
-        "q": 17,
-        "r": 18,
-        "s": 19,
-        "t": 20,
-        "u": 21,
-        "v": 22,
-        "w": 23,
-        "x": 24,
-        "y": 25,
-        "z": 26
-    };
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     const letters = string.split('');
-    const result = [];
-    for (let letter of letters) {
-        result.push(alphabet[letter]);
-    };
-    return result.reduce((a, b) => a + b);
+    return letters.reduce((acc, letter) => (acc += (alphabet.indexOf(letter) + 1), acc), 0);
 }
 
 console.log(wordsToMarks("attitude"), 100, wordsToMarks("attitude") === 100);

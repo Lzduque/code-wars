54613f73ebe62173697fcebf1665a6a6919aa2d8
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-06-16 13:43:17 -0400

    code wars daily exercise

---
+++ nbDig.js
@@ -0,0 +1,15 @@
+function nbDig(n, d) {
+    const squared = [];
+    for (let i = 0; i <= n; i++) {
+        squared.push(Math.pow(i, 2));
+    }
+    const digits = squared.map(number => number.toString().split(''))
+    const flatten = [].concat(...digits);
+    const numOfDs = flatten.filter(n => Number(n) === d).length;
+    return numOfDs;
+}
+
+console.log(nbDig(5750, 0), 4700);
+console.log(nbDig(11011, 2), 9481);
+console.log(nbDig(12224, 8), 7733);
+console.log(nbDig(11549, 1), 11905);

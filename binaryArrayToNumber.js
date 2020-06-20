20372a7558fcfa356e37b9640f0a67d1fa51831b
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-06-20 10:45:53 -0400

    Update binaryArrayToNumber.js

--- binaryArrayToNumber.js
+++ binaryArrayToNumber.js
@@ -1,13 +1,6 @@
 const binaryArrayToNumber = arr => {
-    const binary = arr.reverse();
-    const result = [];
-    for (let i = 0; i < binary.length; i++) {
-        if (binary[i] === 1) {
-            result.push(Math.pow(2, i));
-        }
-    }
-    const sum = result.reduce((acc, current) => acc + current, 0);
-    return sum;
+    const binary = arr.join("");
+    return parseInt(binary, 2);;
 };
 
 console.log(binaryArrayToNumber([0, 0, 0, 1]), 1);

a18dd72cde75fafe190117dfe374c119958c7685
Leticia <lzduque@hotmail.com>
2020-06-22 14:20:00 -0400

    Update domainName.js

--- domainName.js
+++ domainName.js
@@ -1,20 +1,14 @@
 function domainName(url) {
+  const www = url.split('www.');
   let domain;
-  if (url.includes('http://www.')) {
-    domain = url.slice(11);
-  } else if (url.includes('http://')) {
-    domain = url.slice(7);
-  } else if (url.includes('https://www.')) {
-    domain = url.slice(12);
-  } else if (url.includes('https://')) {
-    domain = url.slice(8);
-  } else if (url.includes('www.')) {
-    domain = url.slice(4);
-  } else {
-    domain = url;
-  }
+  www.length >= 2 ? domain = www[1] : domain = www[0];
+
+  const http = domain.split('//');
+  http.length >= 2 ? domain = http[1] : domain = http[0];
+
   return domain.split('.')[0];
 }
+
 
 console.log(domainName("http://google.com"), "google");
 console.log(domainName("http://google.co.jp"), "google");

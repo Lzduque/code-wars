a09f6687bb612e9e6ebfc33a52fd7ff838796421
Leticia <lzduque@hotmail.com>
2020-06-25 13:26:07 -0400

    code wars daily exercise

---
+++ findOdd.js
@@ -0,0 +1,22 @@
+function findOdd(A) {
+  const counts = {};
+  for (let num of A){
+    if(!counts[num]) {
+      counts[num] = 1;
+    } else {
+      counts[num] += 1;
+    }
+  }
+  for (let num in counts) {
+    if (counts[num] % 2 !== 0) {
+      return Number(num);
+    }
+  }
+}
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]), 5);
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]), -1);
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]), 5);
+console.log(findOdd([10]), 10);
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]), 10);
+console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]), 1);

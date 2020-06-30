a1239a316a7e068c6b9e6a98eba52d8411e861a4
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-06-30 14:34:31 -0400

    code wars daily exercise

---
+++ accum.js
@@ -0,0 +1,15 @@
+function accum(s) {
+    const letters = s.toLowerCase().split('');
+    const acc = [];
+    for (let i = 0; i < letters.length; i++) {
+        acc.push(letters[i].repeat(i + 1));
+    }
+    const result = acc.map((letters) => letters[0].toUpperCase() + letters.slice(1));
+    return result.join('-');
+}
+
+console.log(accum("ZpglnRxqenU") === "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+console.log(accum("NyffsGeyylB") === "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+console.log(accum("MjtkuBovqrU") === "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+console.log(accum("EvidjUnokmM") === "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+console.log(accum("HbideVbxncC") === "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
